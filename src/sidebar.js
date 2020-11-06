bb1b7a09db2d05633d7d7f6018ed40556ac883d4
Mikko Reinikainen <mikko.reinikainen@reaktor.com>
2020-11-06 16:40:12 +0200

    Improve list of selected stickers and sidebar visuals

--- src/sidebar.js
+++ src/sidebar.js
@@ -1,19 +1,22 @@
 const errorElement = document.getElementById('day-counter-error')
+const dayCounterInsertElement = document.getElementById('day-counter-insert')
 
 async function insertDayCounters() {
   errorElement.innerText = ''
+  dayCounterInsertElement.className = 'miro-input-field'
   const startDate = new Date(document.getElementById('day-counter-start-date').value)
   const descriptionBefore = document.getElementById('day-counter-description-before').value
   const descriptionAfter = document.getElementById('day-counter-description-after').value
   const selectedWidgets = await miro.board.selection.get()
 
   if (!selectedWidgets || selectedWidgets.length === 0) {
+    dayCounterInsertElement.className += ' miro-input-field--invalid'
     errorElement.innerText = 'Please select a sticker for inserting day counter.'
     return
   }
   selectedWidgets.forEach(widget => {
     if (typeof widget.text === 'string') {
-      widget.metadata[APP_ID] = { startDate, descriptionBefore, descriptionAfter }
+      widget.metadata[APP_ID] = {startDate, descriptionBefore, descriptionAfter}
       widget.capabilities.editable = false
     }
   })
@@ -25,22 +28,31 @@ async function getSelectedWidgets() {
   const selectedWidgets = await miro.board.selection.get()
   const dayCounters = []
   selectedWidgets.forEach(widget => {
-    if (widget.metadata[APP_ID]) {
-      const { startDate: startDateStr, descriptionBefore, descriptionAfter } = widget.metadata[APP_ID]
-      if (startDateStr) {
-        const startDate = new Date(startDateStr)
-        const days = startDate ? getDaysNow(startDate) : ''
-        const text = getText(days, descriptionBefore, descriptionAfter)
-        dayCounters.push({startDate, text})
-      }
+    const stickerBackgroundColor = widget.style.stickerBackgroundColor
+    if (widget.metadata[APP_ID] && widget.metadata[APP_ID].startDate) {
+      const {startDate: startDateStr, descriptionBefore, descriptionAfter} = widget.metadata[APP_ID]
+      const startDate = new Date(startDateStr)
+      const days = startDate ? getDaysNow(startDate) : ''
+      const text = getText(days, descriptionBefore, descriptionAfter)
+      dayCounters.push({startDate, text, stickerBackgroundColor})
+    } else {
+      dayCounters.push({text: widget.plainText, stickerBackgroundColor})
     }
   })
 
   const widgetInfoElement = document.getElementById('day-counter-widget-info')
   widgetInfoElement.innerHTML = dayCounters.length === 0
-    ? 'No selected day counter stickers.'
-    : '<ul>' + dayCounters.map(
-    dayCounter => '<li>' + dayCounter.startDate.toISOString() + ': ' + dayCounter.text + '</li>'
+    ? '<span style="color: #827F9B">No selected stickers.</span>'
+    : '<ul style="list-style: none;">' + dayCounters.map(
+    dayCounter => '<li>' +
+      '<span style="font-size: xx-large; color: ' + dayCounter.stickerBackgroundColor + '">■</span>' +
+      '<br />' +
+      dayCounter.text + '' +
+      '<br />' +
+      (dayCounter.startDate
+        ? '(day counter starts at ' + dayCounter.startDate.toISOString().split('T')[0] + ')'
+        : '(no day counter)') +
+      '</li>'
   ).join('') + '</ul>'
 }
 
